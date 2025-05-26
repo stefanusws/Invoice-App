52e4c655e2507ffe3b3f48fe5e095ba7cec57cb4
stefanusws <stefanusws@yahoo.co.id>
2025-05-26 12:29:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -21,4 +21,4 @@ def submit():
     return render_template('invoice.html', data=data, now=datetime.now())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8000)
